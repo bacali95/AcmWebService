5a0258a4b74fc5503e9d6fba3d307bcffac5284f
Nesrine Sghaier <nesrinesghaier10@gmail.com>
2018-10-02 17:05:46 +0100

    branch project

--- frontend/src/main/frontend/src/app/acmer/routing/acmer-routing.module.ts
+++ frontend/src/main/frontend/src/app/acmer/routing/acmer-routing.module.ts
@@ -4,12 +4,14 @@ import {AcmerListComponent} from "../component/acmer-list/acmer-list.component";
 import {AcmerAddComponent} from "../component/acmer-add/acmer-add.component";
 import {AcmerEditComponent} from "../component/acmer-edit/acmer-edit.component";
 import {DataTableComponent} from "../component/data-table/data-table.component";
+import {AcmerDetailsComponent} from "../component/acmer-details/acmer-details.component";
 
 const routes: Routes = [
   {path: 'acmers', component: AcmerListComponent},
   {path: 'acmers/create', component: AcmerAddComponent},
-  {path: 'acmers/login', component: DataTableComponent},
-  {path: 'acmers/edit/:handle', component: AcmerEditComponent}
+  //{path: 'acmers/login', component: DataTableComponent},
+  {path: 'acmers/edit/:handle', component: AcmerEditComponent},
+  {path: 'acmers/details/:handle', component: AcmerDetailsComponent}
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
